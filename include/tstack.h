33fee3fe3430b484c91aeecf55350e7ddd06fbba
momchik5 <160311115+momchik5@users.noreply.github.com>
2024-04-10 01:45:07 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,48 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+#include <iostream>
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+T* array;
+int top;
+public:
+TStack() : top(0) {
+array = new T[size];
+}
+T pop() {
+    if (top == 0) {
+        throw "Empty!";
+    }
+    else {
+        top -= 1;
+        return array[top];
+    }
+}
+
+void push(T item) {
+    array[top] = item;
+    top++;
+}
+
+T get() const {
+    return array[top];
+}
+
+bool isempty() {
+    return top == 0;
+}
+
+T check() {
+    if (top == 0) {
+        throw "Empty!";
+    }
+    else {
+        return array[top - 1];
+    }
+}
 };
 
 #endif  // INCLUDE_TSTACK_H_
