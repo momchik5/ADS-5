a278ba8033cf5397d562ef0047202938ba40e88b
momchik5 <160311115+momchik5@users.noreply.github.com>
2024-04-10 03:48:54 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -8,17 +8,17 @@ int prioritet(char);
 std::string infx2pstfx(std::string inf) {
     std::string postfix = "";
     TStack<char, 100> d;
-    for (int i = 0; i < infix.length(); i++) {
-        if (isdigit(infix[i])) {
-            postfix += infix[i];
-            if (i != infix.length() - 1) {
+    for (int i = 0; i < inf.length(); i++) {
+        if (isdigit(inf[i])) {
+            postfix += inf[i];
+            if (i != inf.length() - 1) {
                 postfix += " ";
             }
-        } else if (infix[i] == '(') {
-            d.push(infix[i]);
+        } else if (inf[i] == '(') {
+            d.push(inf[i]);
         } else if (d.isempty()) {
-            d.push(infix[i]);
-        } else if (infix[i] == ')') {
+            d.push(inf[i]);
+        } else if (inf[i] == ')') {
             while (true) {
                 if (d.isempty()) {
                     break;
@@ -26,26 +26,26 @@ std::string infx2pstfx(std::string inf) {
                     break;
                 }
                 postfix += d.pop();
-                if (i != infix.length() - 1) {
+                if (i != inf.length() - 1) {
                     postfix += " ";
                 }
             }
             d.pop();
-        } else if (prioritet(infix[i]) > prioritet(d.check())) {
-            d.push(infix[i]);
+        } else if (prioritet(inf[i]) > prioritet(d.check())) {
+            d.push(inf[i]);
         } else {
        while (true) {
             if (d.isempty()) {
                 break;
-            } else if (!(prioritet(infix[i]) <= prioritet(d.check()))) {
+            } else if (!(prioritet(inf[i]) <= prioritet(d.check()))) {
                 break;
        }
         postfix += d.pop();
-        if (i != infix.length() - 1) {
+        if (i != inf.length() - 1) {
             postfix += " ";
         }
     }
-    d.push(infix[i]);
+    d.push(inf[i]);
 }
 }
 while (!d.isempty()) {
@@ -63,7 +63,7 @@ int eval(std::string post) {
     if (isdigit(post[i])) {
       str += post[i];
     } else if (str.length() && post[i] == ' ') {
-      operands.push(atoi(time.c_str()));
+      operands.push(atoi(str.c_str()));
       str = "";
     } else {
       switch (post[i]) {
